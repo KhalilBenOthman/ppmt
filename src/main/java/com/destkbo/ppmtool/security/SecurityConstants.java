3c76f6a4e186227d8f2d670a788ba2ba36dff40b
KhalilBenOthman <khalil.b.othman@gmail.com>
2021-08-15 16:59:00 +0200

    xxx

--- src/main/java/com/destkbo/ppmtool/security/SecurityConstants.java
+++ src/main/java/com/destkbo/ppmtool/security/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants {
 	public static final String SECRET ="SecretKeyToGenJWTs";
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING ="Authorization";
-	public static final long EXPIRATION_TIME = 600_000; //30s
+	public static final long EXPIRATION_TIME = 6000000; //30s
 	
 }
